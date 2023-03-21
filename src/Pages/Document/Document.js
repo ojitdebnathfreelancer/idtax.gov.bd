95cdcecc0d8de3ca476a05f8c1f7b582e6cb4399
unknown <ojitdebnathfreelancer.com>
2023-03-22 04:53:21 +0600

    login and footer done

--- src/Pages/Document/Document.js
+++ src/Pages/Document/Document.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Document = () => {
+    document.title = "ভূমি উন্নয়ন কর সিস্টেম অটোমেশন: LdtaxHoldings";
     return (
         <div>
             <h1>This is document table page</h1>
